0fbf9d059e8770edfe514c496d97abf27961fd40
sicily <sicily.wang@outlook.com>
2020-05-07 15:39:51 +0300

    add comments

--- src/snake.h
+++ src/snake.h
@@ -6,33 +6,41 @@
 
 class Snake {
  public:
+  // Define snake's moving direction
   enum class Direction { kUp, kDown, kLeft, kRight };
 
+  // Constructor
   Snake(int grid_width, int grid_height)
       : grid_width(grid_width),
         grid_height(grid_height),
         head_x(grid_width / 2),
         head_y(grid_height / 2) {}
 
-  void Update();
+  
+  void Update(); // Update snake's head and body
 
-  void GrowBody();
-  bool SnakeCell(int x, int y);
+  // getter and setter
+  void GrowBody(){ growing = true; };
 
-  Direction direction = Direction::kUp;
+  bool SnakeCell(int x, int y); // flag indicating whether the cell is occupied by snake or not
 
-  float speed{0.1f};
-  int size{1};
-  bool alive{true};
-  float head_x;
+
+  Direction direction = Direction::kUp; // Initialize snake's moving direction as kUp
+
+  float speed{0.1f}; // Initialize moving speed as 0.1f
+  int size{1}; // Initialize snake size as 1
+  bool alive{true}; // flag indicating whether snake is alive or not
+  float head_x; 
   float head_y;
+
   std::vector<SDL_Point> body;
 
  private:
-  void UpdateHead();
-  void UpdateBody(SDL_Point &current_cell, SDL_Point &prev_cell);
+  void UpdateHead(); // Update snake's head 
+  void UpdateBody(SDL_Point &current_cell, SDL_Point &prev_cell); //  Update snake's status: growing, not growing or dead
 
-  bool growing{false};
+  //private members
+  bool growing{false}; //flag indicating snake which is not growing
   int grid_width;
   int grid_height;
 };
