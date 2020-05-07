2aabbd9c942480a3d13449959923735e47e16f51
sicily <sicily.wang@outlook.com>
2020-05-07 16:01:19 +0300

    add comments to class member functions

--- src/game.h
+++ src/game.h
@@ -9,25 +9,31 @@
 
 class Game {
  public:
+  // Constructor
   Game(std::size_t grid_width, std::size_t grid_height);
+
+  // Behaviour methods
   void Run(Controller const &controller, Renderer &renderer,
            std::size_t target_frame_duration);
-  int GetScore() const;
-  int GetSize() const;
+
+  // getters / setters        
+  int GetScore() const { return score; };
+  int GetSize() const { return snake.size; };
 
  private:
-  Snake snake;
-  SDL_Point food;
+  Snake snake; // Make an instance of snake object
+  SDL_Point food; // Define food as SDL_Point
 
+  // Pick width and height at random
   std::random_device dev;
   std::mt19937 engine;
   std::uniform_int_distribution<int> random_w;
   std::uniform_int_distribution<int> random_h;
 
-  int score{0};
+  int score{0}; // Initialize score as 0
 
-  void PlaceFood();
-  void Update();
+  void PlaceFood(); // Return food coordinates
+  void Update(); // Update snake(grow and speed), food and score
 };
 
 #endif
