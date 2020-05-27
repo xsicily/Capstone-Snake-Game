3d913e5b8ed4ca37b1823f192e2f1bdffd159c83
sicily <sicily.wang@outlook.com>
2020-05-27 14:44:50 +0300

    change raw pointer to unique pointer

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,6 +1,8 @@
 #include "renderer.h"
 #include <iostream>
 #include <string>
+#include <memory>
+#include <SDL2/SDL.h> 
 
 Renderer::Renderer(const std::size_t screen_width,
                    const std::size_t screen_height,
@@ -16,6 +18,7 @@ Renderer::Renderer(const std::size_t screen_width,
   }
 
   // Create Window
+  /* 
   sdl_window = SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED,
                                 SDL_WINDOWPOS_CENTERED, screen_width,
                                 screen_height, SDL_WINDOW_SHOWN);
@@ -31,10 +34,27 @@ Renderer::Renderer(const std::size_t screen_width,
     std::cerr << "Renderer could not be created.\n";
     std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
   }
+  */
+
+  sdl_window.reset(SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED,
+                                SDL_WINDOWPOS_CENTERED, screen_width,
+                                screen_height, SDL_WINDOW_SHOWN));
+
+  if (nullptr == std::move(sdl_window)) {
+    std::cerr << "Window could not be created.\n";
+    std::cerr << " SDL_Error: " << SDL_GetError() << "\n";
+  }
+
+  // Create renderer
+  sdl_renderer.reset(SDL_CreateRenderer(sdl_window.get(), -1, SDL_RENDERER_ACCELERATED));
+  if (nullptr == std::move(sdl_renderer)) {
+    std::cerr << "Renderer could not be created.\n";
+    std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
+  }
 }
 
 Renderer::~Renderer() {
-  SDL_DestroyWindow(sdl_window);
+  //SDL_DestroyWindow(sdl_window);
   SDL_Quit();
 }
 
@@ -44,38 +64,40 @@ void Renderer::Render(Snake const snake, SDL_Point const &food) {
   block.h = screen_height / grid_height;
 
   // Clear screen
-  SDL_SetRenderDrawColor(sdl_renderer, 0x1E, 0x1E, 0x1E, 0xFF);
-  SDL_RenderClear(sdl_renderer);
+  //int SDL_SetRenderDrawColor(SDL_Renderer* renderer,r,g,b,a)
+
+  SDL_SetRenderDrawColor(sdl_renderer.get(), 0x1E, 0x1E, 0x1E, 0xFF);
+  SDL_RenderClear(sdl_renderer.get());
 
   // Render food
-  SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xCC, 0x00, 0xFF);
+  SDL_SetRenderDrawColor(sdl_renderer.get(), 0xFF, 0xCC, 0x00, 0xFF);
   block.x = food.x * block.w;
   block.y = food.y * block.h;
-  SDL_RenderFillRect(sdl_renderer, &block);
+  SDL_RenderFillRect(sdl_renderer.get(), &block);
 
   // Render snake's body
-  SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+  SDL_SetRenderDrawColor(sdl_renderer.get(), 0xFF, 0xFF, 0xFF, 0xFF);
   for (SDL_Point const &point : snake.body) {
     block.x = point.x * block.w;
     block.y = point.y * block.h;
-    SDL_RenderFillRect(sdl_renderer, &block);
+    SDL_RenderFillRect(sdl_renderer.get(), &block);
   }
 
   // Render snake's head
   block.x = static_cast<int>(snake.head_x) * block.w;
   block.y = static_cast<int>(snake.head_y) * block.h;
   if (snake.alive) {
-    SDL_SetRenderDrawColor(sdl_renderer, 0x00, 0x7A, 0xCC, 0xFF);
+    SDL_SetRenderDrawColor(sdl_renderer.get(), 0x00, 0x7A, 0xCC, 0xFF);
   } else {
-    SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0x00, 0x00, 0xFF);
+    SDL_SetRenderDrawColor(sdl_renderer.get(), 0xFF, 0x00, 0x00, 0xFF);
   }
-  SDL_RenderFillRect(sdl_renderer, &block);
+  SDL_RenderFillRect(sdl_renderer.get(), &block);
 
   // Update Screen
-  SDL_RenderPresent(sdl_renderer);
+  SDL_RenderPresent(sdl_renderer.get());
 }
 
 void Renderer::UpdateWindowTitle(int score, int fps) {
   std::string title{"Snake Score: " + std::to_string(score) + " FPS: " + std::to_string(fps)};
-  SDL_SetWindowTitle(sdl_window, title.c_str());
+  SDL_SetWindowTitle(sdl_window.get(), title.c_str());
 }
--- src/renderer.h
+++ src/renderer.h
@@ -2,8 +2,10 @@
 #define RENDERER_H
 
 #include <vector>
-#include "SDL.h"
+#include <SDL2/SDL.h> 
 #include "snake.h"
+#include <memory>
+#include "shared.h"
 
 class Renderer {
  public:
@@ -16,10 +18,27 @@ class Renderer {
   void Render(Snake const snake, SDL_Point const &food);// Render snake and food
   void UpdateWindowTitle(int score, int fps);// Update window title: snake score and FPS
 
+ 
+
  private:
   //
+  /*
   SDL_Window *sdl_window;
   SDL_Renderer *sdl_renderer;
+  */
+  struct sdl_deleter
+  {
+    void operator()(SDL_Window *p) const{
+      SDL_DestroyWindow(p);
+    }
+    void operator()(SDL_Renderer *p) const{
+      SDL_DestroyRenderer(p);
+    }
+  };
+  std::unique_ptr<SDL_Window, sdl_deleter> sdl_window;
+  std::unique_ptr<SDL_Renderer, sdl_deleter> sdl_renderer;
+
+ 
   
   // Member variables
   const std::size_t screen_width;
