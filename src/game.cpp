f86e6f0dc41fde488bbe3d79efa9dd97a78f28a7
sicily <sicily.wang@outlook.com>
2020-05-31 12:01:49 +0300

    modify SDL header file

--- src/game.cpp
+++ src/game.cpp
@@ -1,12 +1,12 @@
 #include "game.h"
 #include <iostream>
-#include "SDL.h"
+#include <SDL2/SDL.h> 
 
 Game::Game(std::size_t grid_width, std::size_t grid_height)
     : snake(grid_width, grid_height),
       engine(dev()),
-      random_w(0, static_cast<int>(grid_width)),
-      random_h(0, static_cast<int>(grid_height)) {
+      random_w(0, static_cast<int>(grid_width - 1)),
+      random_h(0, static_cast<int>(grid_height - 1)) {
   PlaceFood();
 }
 
